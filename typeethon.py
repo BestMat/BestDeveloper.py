23e2b13bc1aa6c8febb3613eeec157633dfc8ec7
BestMat <76582849+BestMat@users.noreply.github.com>
2021-02-28 15:41:46 +0530

    Creating TypeEthon.py
    TypeEthon extends Python by adding types.

---
+++ typeethon.py
@@ -0,0 +1,20 @@
+def isInt(a):
+  if type(a) != int:
+    print("Not a INT")
+def isStr(a):
+  if type(a) != str:
+    print("Not a STRING")
+def isFloat(a):
+  if type(a) != float:
+    print("Not a FLOAT")
+def isList(a):
+  if type(a) != list:
+    print("Not a LIST/ARRAY")
+def isTuple(a):
+  if type(a) != tuple:
+    print("Not a TUPLE")
+def isDict(a):
+  if type(a) != dict:
+    print("Not a JSON/DICT")
+  
+  
